8dbdce7b1bcdf7d6cf27e623a52d4a3e6caa366e
JHJ <90052524+quantumleapj@users.noreply.github.com>
2022-02-25 21:26:39 +0900

    Add files via upload

---
+++ study/Hunger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunger
+{
+    internal class Hungerclass
+    {
+        public static void hunger()
+        {
+            Console.WriteLine(" hunger 출력");
+
+        }
+    }
+}
---
+++ study/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Hunger;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            Console.WriteLine(args);
+            Hunger.Hungerclass.hunger();
+            Console.WriteLine("really??");
+
+        }
+    }
+}
